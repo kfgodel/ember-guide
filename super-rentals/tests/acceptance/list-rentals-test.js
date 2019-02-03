c98df0a0b758a0b90fee8567602af6b278b1a510
Dario L Garcia <dario.garcia@10pines.com>
2019-02-02 22:58:25 -0300

    Complete tests and https://guides.emberjs.com/release/tutorial/autocomplete-component/

--- super-rentals/tests/acceptance/list-rentals-test.js
+++ super-rentals/tests/acceptance/list-rentals-test.js
@@ -1,6 +1,6 @@
 import {module, test} from 'qunit';
 import {setupApplicationTest} from 'ember-qunit';
-import {click, currentURL, visit} from '@ember/test-helpers'
+import {click, currentURL, fillIn, triggerKeyEvent, visit} from '@ember/test-helpers'
 import setupMirage from 'ember-cli-mirage/test-support/setup-mirage';
 
 module('Acceptance | list rentals', function (hooks) {
@@ -30,6 +30,11 @@ module('Acceptance | list rentals', function (hooks) {
   });
 
   test('should filter the list of rentals by city.', async function (assert) {
+    await visit('/');
+    await fillIn('.list-filter input', 'seattle');
+    await triggerKeyEvent('.list-filter input', 'keyup', 69);
+    assert.equal(this.element.querySelectorAll('.results .listing').length, 1, 'should display 1 listing');
+    assert.ok(this.element.querySelector('.listing .location').textContent.includes('Seattle'), 'should contain 1 listing with location Seattle');
   });
 
   test('should show details for a selected rental', async function (assert) {
