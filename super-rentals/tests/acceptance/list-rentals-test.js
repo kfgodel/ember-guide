49fc4af4b4c8473a9286fbc9eaa66215a390e4a4
Dario L Garcia <dario.garcia@10pines.com>
2019-02-02 22:12:58 -0300

    https://guides.emberjs.com/release/tutorial/ember-data/

--- super-rentals/tests/acceptance/list-rentals-test.js
+++ super-rentals/tests/acceptance/list-rentals-test.js
@@ -1,9 +1,11 @@
 import {module, test} from 'qunit';
 import {setupApplicationTest} from 'ember-qunit';
 import {click, currentURL, visit} from '@ember/test-helpers'
+import setupMirage from 'ember-cli-mirage/test-support/setup-mirage';
 
 module('Acceptance | list rentals', function (hooks) {
   setupApplicationTest(hooks);
+  setupMirage(hooks);
 
   test('should show rentals as the home page', async function (assert) {
     await visit('/');
